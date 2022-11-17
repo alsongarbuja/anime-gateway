1692d7a7388a472048ddb7c6310c5a705d8ac82a
alsongarbuja <garbuja12alson@gmail.com>
2022-11-17 19:49:50 +0545

    [css]: add tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    fontFamily: {
+      sans: [ 'Poppins', 'sans-serif' ],
+    },
     extend: {},
   },
+  darkMode: 'class',
   plugins: [],
 }
